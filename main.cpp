1d75135c2500910066c3f5357c1dce1a64c9f7f2
Krunner1015 <kaidenbartrum15@gmail.com>
2025-05-14 14:57:41 -0400

    Initialize and test sfml

--- main.cpp
+++ main.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
+#include <fstream>
+#include <cctype>
+#include <vector>
+#include <random>
+#include <SFML/Graphics.hpp>
+
+void setText(sf::Text &text, float x, float y) {
+    sf::FloatRect textRect = text.getLocalBounds();
+    text.setOrigin(textRect.left + textRect.width/2.0f,
+    textRect.top + textRect.height/2.0f);
+    text.setPosition(sf::Vector2f(x, y));
+}
 
 int main() {
+    sf::Clock clock;
+    sf::Time pausedDuration = sf::Time::Zero;
+    sf::Time pauseStartTime;
+    sf::Time finalTime;
+    sf::Font font;
+    if (!font.loadFromFile("files/font.ttf")) {
+        std::cout << "Error loading font" << std::endl;
+        return -1;
+    }
+    int width = 800;
+    int height = 600;
 
+    sf::RenderWindow window(sf::VideoMode(width, height), "Welcome!", sf::Style::Close);
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Red);
+
+    while(window.isOpen()) {
+        sf::Event event;
+        while(window.pollEvent(event)) {
+            if(event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
     return 0;
 }
