7b64e752bfd7fa286ae7fdb83080af2935858449
Krunner1015 <kaidenbartrum15@gmail.com>
2025-05-14 15:55:51 -0400

    Create a start screen for the game

--- main.cpp
+++ main.cpp
@@ -22,38 +22,66 @@ int main() {
         std::cout << "Error loading font" << std::endl;
         return -1;
     }
-    int width = 800;
-    int height = 600;
+    int width = 1000;
+    int height = 900;
     float speed = 200.0f;
+    bool gameStart = false;
 
-    sf::RenderWindow window(sf::VideoMode(width, height), "Welcome!", sf::Style::Close);
+    sf::Texture introTex;
+    if (!introTex.loadFromFile("files/images/LittlePrinceOnMoon.png")) {
+        std::cout << "Error loading intro image" << std::endl;
+    }
+    sf::Sprite intro;
+    intro.setTexture(introTex);
+
+    sf::RectangleShape button(sf::Vector2f(500.0f, 100.0f));
+    button.setFillColor(sf::Color(171, 222, 105));
+    button.setPosition(sf::Vector2f(width / 2 - 250, height - 150));
+
     sf::CircleShape prince(100.f);
     prince.setFillColor(sf::Color::Black);
     prince.setPosition(sf::Vector2f(width / 2 - 100, height / 2 - 100));
 
-    while(window.isOpen()) {
+    sf::RenderWindow start(sf::VideoMode(width, height), "Welcome!", sf::Style::Close);
+    while(start.isOpen()) {
         sf::Event event;
-        while(window.pollEvent(event)) {
+        while(start.pollEvent(event)) {
             if(event.type == sf::Event::Closed) {
-                window.close();
+                start.close();
             }
         }
+        start.clear();
+        start.draw(intro);
+        start.draw(button);
+        start.display();
+    }
 
-        float deltaTime = clock.restart().asSeconds();
+    if (gameStart) {
+        sf::RenderWindow game(sf::VideoMode(width, height), "The Little Prince", sf::Style::Close);
+        while(game.isOpen()) {
+            sf::Event event;
+            while(game.pollEvent(event)) {
+                if(event.type == sf::Event::Closed) {
+                    game.close();
+                }
+            }
 
-        // Handle player movement
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) // Move up
-            prince.move(0, -speed * deltaTime);
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) // Move down
-            prince.move(0, speed * deltaTime);
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) // Move left
-            prince.move(-speed * deltaTime, 0);
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) // Move right
-            prince.move(speed * deltaTime, 0);
+            float deltaTime = clock.restart().asSeconds();
 
-        window.clear(sf::Color::White);
-        window.draw(prince);
-        window.display();
+            // Handle player movement
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) // Move up
+                prince.move(0, -speed * deltaTime);
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) // Move down
+                prince.move(0, speed * deltaTime);
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) // Move left
+                prince.move(-speed * deltaTime, 0);
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) // Move right
+                prince.move(speed * deltaTime, 0);
+
+            game.clear(sf::Color::White);
+            game.draw(prince);
+            game.display();
+        }
     }
     return 0;
 }
